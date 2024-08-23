5afb9e85cf8127fb35325e4d63b83d0b75e932ad
swayam <swayam@gmail.com>
2024-08-23 09:48:30 +0530

    modification with frontend

--- course_management/settings.py
+++ course_management/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'courses',
     'rest_framework',
+    'corsheaders',
+
 ]
 
 MIDDLEWARE = [
@@ -49,6 +51,8 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
 
 ROOT_URLCONF = 'course_management.urls'
@@ -70,6 +74,10 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'course_management.wsgi.application'
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3002"
+    ]
 
 
 # Database
--- courses/models.py
+++ courses/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Course(models.Model):
     title = models.CharField(max_length=255)
-    course_code = models.CharField(max_length=10)
+    code = models.CharField(max_length=20,unique=True)
     description = models.TextField()
 
     def __str__(self):
